f3d5999a4f83f6da34ed79f40ba859732593b3bf
Jérémie van der Sande <jeremie.vandersande@gmail.com>
2020-09-18 15:57:08 +0200

    [Update] Allow deferred rendering of previously loaded Piece

--- packages/piece/src/index.ts
+++ packages/piece/src/index.ts
@@ -3,13 +3,22 @@ import React from 'react'
 const {document} = global
 
 function load(component: React.ComponentClass|React.FC) {
-  console.log('ChessboardLoaded::' + process.env.CHESSBOARD_PIECE)
   const customEvent = new CustomEvent('ChessboardLoaded::' + process.env.CHESSBOARD_PIECE, {detail: {component}})
   document.dispatchEvent(customEvent)
 }
 
 export function ChessboardPiece(component: React.ComponentClass|React.FC) {
-  load(component)
+  const listener = () => load(component)
+  document.addEventListener('ChessboardNeeded::' + process.env.CHESSBOARD_PIECE, listener)
 
-  return ({ reload: load })
+  listener()
+
+  return ({
+    reload: (c: React.ComponentClass|React.FC) => {
+      document.removeEventListener('ChessboardNeeded::' + process.env.CHESSBOARD_PIECE, listener)
+
+      // Wrap in setTimeout to avoid Stack Overflow
+      setTimeout(() => ChessboardPiece(c), 0)
+    }
+  })
 }
